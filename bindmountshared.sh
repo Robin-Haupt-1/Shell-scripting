be30f4db387f3b3601947ab2dd7bc1022a92490f
Robin-Haupt-1 <robinhaupt@outlook.com>
2022-02-28 21:15:19 +0100

    add first version of "bind mount shared vault into others" script

---
+++ bindmountshared.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+IFS=$'\n'
+
+for d in $(find /hdd/Obsidian -maxdepth 1 -type d); do
+  case "$d" in
+     "/hdd/Obsidian/\$RECYCLE.BIN") continue
+	;;
+     "/hdd/Obsidian/System Volume Information") continue
+	;;
+     "/hdd/Obsidian/_shared") continue
+	;;
+     "/hdd/Obsidian/.Trash-1000") continue
+	;;
+     "/hdd/Obsidian") continue
+	;;
+     "/hdd/Obsidian/Automatic MOC sample") continue
+	;;
+     "/hdd/Obsidian/_blank vault") continue
+	;;
+     "/hdd/Obsidian/Plugin Development") continue
+	;;
+	esac
+    mkdir -p "$d/__shared"
+    sudo /usr/bin/mount --bind "/hdd/Obsidian/_shared/_shared/" "$d/__shared/"
+    echo "$d"
+done
+
